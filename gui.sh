94c1bf34229df4f4aa27fe31b20d12697c1436c1
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-11-04 23:47:02 -0500

    Use wget and Fix url for chrome

--- gui.sh
+++ gui.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get install ttf-unfonts-core
 
 # google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable-current_amd64.deb
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb \
 	&& apt-get install -f \
 	&& dpkg -i google-chrome-stable_current_amd64.deb
