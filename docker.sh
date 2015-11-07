b9d8a097f387473dabd6a6818d5aabc9e3cf9849
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-11-06 23:13:42 -0500

    Add docker-compose

--- docker.sh
+++ docker.sh
@@ -6,4 +6,9 @@ set -e
 # install docker
 curl -sSL https://get.docker.com | sh
 
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.5.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # vim: set filetype=sh :
