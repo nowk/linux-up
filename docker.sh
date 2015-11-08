3aa19b6a81ca296fd0fe7fe99738dff4a9a46009
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-11-08 01:21:28 -0500

    terraform, packer

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# vim: set filetype=sh :
 set -e
 
 # must setup /etc/fstab before running docker
@@ -11,4 +12,15 @@ curl -L https://github.com/docker/compose/releases/download/1.5.0/docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# vim: set filetype=sh :
+# install terraform
+cd /opt
+sudo mkdir -p /opt/terraform
+wget https://releases.hashicorp.com/terraform/0.6.6/terraform_0.6.6_linux_amd64.zip
+sudo unzip terraform_0.6.6_linux_amd64.zip -d /opt/terraform/
+
+# install packer
+cd /opt
+sudo mkdir -p /opt/packer
+wget https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip
+sudo unzip packer_0.8.6_linux_amd64.zip -d /opt/packer/
+
